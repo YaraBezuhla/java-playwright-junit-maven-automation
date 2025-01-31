efdcfe6c0b222e155cc8152150f3c71faeccdbfd
Yaroslava <ybezuhla@gmail.com>
2025-01-31 19:16:25 +0200

    Starting api testing with playwright

---
+++ src/main/java/journal/reading/automation/api/ApiMethods.java
@@ -0,0 +1,51 @@
+package journal.reading.automation.api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.RequestOptions;
+
+import java.io.File;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApiMethods {
+
+    private Playwright playwright;
+    private APIRequestContext request;
+
+    private static final String BOOKS_JSON_FILE = "C:/Users/Ярослава/Projects/playwright-automation-reading-journal/src/test/resources/testData/books.json";
+
+    public void createAPIRequestContext(int bookIndex) {
+
+        try {
+            playwright = Playwright.create();
+            Map<String, String> headers = new HashMap<>();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(new File(BOOKS_JSON_FILE)); // Читання JSON у дерево об'єктів
+            JsonNode selectedBook = jsonNode.get(bookIndex);
+            String requestBody = objectMapper.writeValueAsString(selectedBook);  // Перетворення JsonNode у JSON-рядок
+
+
+            headers.put("Content-Type", "application/json");
+            request = playwright.request().newContext(new APIRequest.NewContextOptions()
+                    .setBaseURL("http://localhost:5000/api")
+                    .setExtraHTTPHeaders(headers));
+
+            APIResponse newBook = request.post("/books", RequestOptions.create().setData(requestBody));
+            //assertTrue(newBook.ok());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Помилка під час обробки JSON або виконання запиту");
+        }
+    }
+}
+
