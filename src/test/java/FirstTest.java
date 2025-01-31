19dee2b1d8bcbaa18ad5a9d197eb992186363eca
Yaroslava <ybezuhla@gmail.com>
2025-01-31 19:16:25 +0200

    Starting api testing with playwright

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,10 +1,12 @@
 import journal.reading.automation.LaunchSettings;
+import journal.reading.automation.api.ApiMethods;
 import org.junit.jupiter.api.Test;
 
-public class FirstTest extends LaunchSettings {
+public class FirstTest  {
 
     @Test
     public void firstTest() {
-
+        ApiMethods apiMethods = new ApiMethods();
+        apiMethods.createAPIRequestContext(0);
     }
 }
