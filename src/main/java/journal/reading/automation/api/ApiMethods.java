77b6659c65bc7426c7da7f21bdf1c59a82663f11
Yaroslava <ybezuhla@gmail.com>
2025-02-01 12:33:05 +0200

    Fixed a bug in the api method and added tests

--- src/main/java/journal/reading/automation/api/ApiMethods.java
+++ src/main/java/journal/reading/automation/api/ApiMethods.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.RequestOptions;
-
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +19,7 @@ public class ApiMethods {
 
     private static final String BOOKS_JSON_FILE = "C:/Users/Ярослава/Projects/playwright-automation-reading-journal/src/test/resources/testData/books.json";
 
-    public void createAPIRequestContext(int bookIndex) {
+    public void createAPIRequestContext(int bookIndex, int codeResponse) {
 
         try {
             playwright = Playwright.create();
@@ -30,14 +30,14 @@ public class ApiMethods {
             JsonNode selectedBook = jsonNode.get(bookIndex);
             String requestBody = objectMapper.writeValueAsString(selectedBook);  // Перетворення JsonNode у JSON-рядок
 
-
             headers.put("Content-Type", "application/json");
             request = playwright.request().newContext(new APIRequest.NewContextOptions()
-                    .setBaseURL("http://localhost:5000/api")
+                    .setBaseURL("http://localhost:5000")
                     .setExtraHTTPHeaders(headers));
 
-            APIResponse newBook = request.post("/books", RequestOptions.create().setData(requestBody));
-            //assertTrue(newBook.ok());
+            APIResponse response = request.post("/api/books", RequestOptions.create().setData(requestBody));
+            int codeResponseActual = response.status();
+            assertEquals(codeResponse, codeResponseActual);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Помилка під час обробки JSON або виконання запиту");
