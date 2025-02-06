bc973fc5811d427aa4946174ffc339349f8e96f6
Yaroslava <ybezuhla@gmail.com>
2025-02-06 17:36:14 +0200

    Fix setting

--- src/test/java/HomePageTests.java
+++ src/test/java/HomePageTests.java
@@ -1,14 +1,12 @@
 import journal.reading.automation.LaunchSettings;
-import journal.reading.automation.database.GetDataFromMongoDB;
 import journal.reading.automation.pageObjects.HomePageObjects;
 import org.junit.jupiter.api.Test;
 
 public class HomePageTests extends LaunchSettings {
-    GetDataFromMongoDB getDataFromMongoDB = new GetDataFromMongoDB();
-    HomePageObjects homePage = new HomePageObjects(page);
 
     @Test
     public void firstTest(){
+        HomePageObjects homePage = new HomePageObjects(page);
         homePage.assertBlocksName();
     }
 }
